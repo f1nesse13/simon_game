35d71fc126aa74df9f0c9c8f46f5c21435ccedce
f1nesse13 <philly.teg@gmail.com>
2019-01-21 08:48:40 -0500

    take_turn & play_game implemented

--- lib/simon.rb
+++ lib/simon.rb
@@ -10,19 +10,34 @@ class Simon
   end
 
   def play
-
+    until game_over
+      self.take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+      show_sequence
+      check_seq = require_sequence
+      round_success_message
+      @sequence_length += 1
   end
 
   def show_sequence
-
-  end
+    add_random_color
+    @seq.join(" ")
+  end 
 
   def require_sequence
-
+    puts "Enter the sequence of colors"
+    @seq.each do |char|
+      input = gets.chomp
+      if input != char
+        @game_over = true
+      end
+    end
+    @seq
   end
 
   def add_random_color
@@ -30,14 +45,17 @@ class Simon
   end
 
   def round_success_message
-
+    puts "You entered the correct sequence! Increasing sequence by 1"
   end
 
   def game_over_message
-
+    puts "Sequence was incorrect! The correct sequence was #{@seq.join(" ")}"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
+
 end
